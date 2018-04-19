dffc7b3f59163fb2b222eb09feba205ac612a1c3
robinpalmqvist <robin.palmqvist@gmail.com>
2018-04-19 07:30:42 +0200

    Camera and Controller scripting

--- WoodoRagDolls/Assets/Scripts/GroundState.cs
+++ WoodoRagDolls/Assets/Scripts/GroundState.cs
@@ -7,16 +7,37 @@ public class GroundState : State {
 
     private PlayerController _controller;
 
+    [Header("Jumping")]
     public MinMaxFloat JumpHeight;
 
+    [Header("Movement")]
+    public float MoveSpeed;
 
-	public override void Initialize(Controller owner)
+    private Vector3 _groundNormal;
+
+
+
+
+    private Vector3 ForwardAlongGround
+    {
+        get
+        {
+            float y = Camera.main.transform.rotation.eulerAngles.y;
+            // float y = transform.rotation.eulerAngles.y;
+            return Quaternion.Euler(90f, y, 0.0f) * _groundNormal;
+        }
+    }
+
+
+    public override void Initialize(Controller owner)
 	{
         _controller = (PlayerController)owner;
+        
 	}
 
 	public override void Enter()
 	{
+       
         
 	}
 
@@ -27,7 +48,34 @@ public class GroundState : State {
 
 	public override void Update()
 	{
+        UpdateMovement();
+       
+
+    }
+
+    private void UpdateMovement()
+    {
+        Vector3 input = _controller.Input;
+        Debug.Log(input);
+        _controller.controller.Move(input * Time.deltaTime * MoveSpeed);
         
-	}
+        Vector3 cameraForward = Camera.main.transform.forward;
+        cameraForward.y = 0.0f;
+
+
+        float angle = Vector3.SignedAngle(input, cameraForward, Vector3.up);
+        Debug.Log(angle);
+        _controller.transform.rotation = Quaternion.Euler(0, -angle, 0);
+        //Vector3 delta = Quaternion.AngleAxis(angle, -_groundNormal) * ForwardAlongGround * MoveSpeed * Time.deltaTime;
+        
+        //Debug.Log(angle);
+        
+       
+    }
+
+    private void UpdateGroundNormal()
+    {
+        
+    }
 
 }
--- WoodoRagDolls/Assets/Scripts/PlayerController.cs
+++ WoodoRagDolls/Assets/Scripts/PlayerController.cs
@@ -6,31 +6,28 @@ using System;
 public class PlayerController : Controller {
 
     public CharacterController controller;
-    public Rigidbody rb;
-    private float _force = 20;
+    public Transform transform;
+   // public Rigidbody rb;
+    //private float _force = 20;
 
     [Range(0f, 1f)] public float InputRequiredToMove = 0.3f;
 
-   /* public Vector3 Input
+    public Vector3 Input
     {
         get
         {
             Vector3 input = new Vector3(UnityEngine.Input.GetAxisRaw("Horizontal"), 0.0f, UnityEngine.Input.GetAxisRaw("Vertical"));
             float y = Camera.main.transform.rotation.eulerAngles.y;
             input = Quaternion.Euler(0f, y, 0f) * input;
-            return input;
+            return input.normalized;
             //Kan (borde?) normaliseras
         }
     }
-    */
 
-	private void Update()
-	{
 
-        Vector3 move = new Vector3(Input.GetAxis("Vertical"), 0f, Input.GetAxis("Horizontal"));
-        rb.AddForce(move * _force);
+    
 
-	}
+	
 
 
 }
