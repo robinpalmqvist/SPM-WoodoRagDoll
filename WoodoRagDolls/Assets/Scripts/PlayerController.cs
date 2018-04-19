c835be20f82925469b064d773a17fb084cae4dc3
robinpalmqvist <robin.palmqvist@gmail.com>
2018-04-20 00:22:28 +0200

    Konstigt script

--- WoodoRagDolls/Assets/Scripts/PlayerController.cs
+++ WoodoRagDolls/Assets/Scripts/PlayerController.cs
@@ -17,33 +17,25 @@ public class PlayerController : Controller {
         get
         {
             Vector3 input = new Vector3(UnityEngine.Input.GetAxisRaw("Horizontal"), 0.0f, UnityEngine.Input.GetAxisRaw("Vertical"));
-<<<<<<< HEAD
-            //float y = Camera.main.transform.rotation.eulerAngles.y;
-            //input = Quaternion.Euler(0f, y, 0f) * input;
+
             return input;
-=======
+
             float y = Camera.main.transform.rotation.eulerAngles.y;
             input = Quaternion.Euler(0f, y, 0f) * input;
             return input.normalized;
-<<<<<<< HEAD
-            
-=======
->>>>>>> ddebe0bf26d1e2c92a14c8dce5754af97b5ab9c7
-            //Kan (borde?) normaliseras
->>>>>>> 1d8ad50cdbc91c28008ee935a80fb4cff4ad3ac1
+
+
         }
     }
 
 
-<<<<<<< HEAD
-	private void Update()
-	{
-        rb.AddForce(Input * 20f);
-=======
-    
->>>>>>> ddebe0bf26d1e2c92a14c8dce5754af97b5ab9c7
 
-	
+    private void Update()
+    {
+        //rb.AddForce(Input * 20f);
+
+
+    }
 
 
 }
