da065496f312d360a9693174d4c905e3ac0f26a9
Ted Hammarlund <TedHammarlund@gmail.com>
2018-04-20 09:35:43 +0200

    PlayerController

--- WoodoRagDolls/Assets/Scripts/PlayerController.cs
+++ WoodoRagDolls/Assets/Scripts/PlayerController.cs
@@ -17,42 +17,15 @@ public class PlayerController : Controller {
     {
         get
         {
-<<<<<<< HEAD
+
             Vector3 input = new Vector3(UnityEngine.Input.GetAxisRaw("LeftStickHorizontal"), 0.0f, UnityEngine.Input.GetAxisRaw("LeftStickVertical"));
 
-            //float y = Camera.main.transform.rotation.eulerAngles.y;
-            //input = Quaternion.Euler(0f, y, 0f) * input;
-            //return input;
-=======
-            Vector3 input = new Vector3(UnityEngine.Input.GetAxisRaw("Horizontal"), 0.0f, UnityEngine.Input.GetAxisRaw("Vertical"));
-
-            return input;
->>>>>>> c835be20f82925469b064d773a17fb084cae4dc3
 
             float y = Camera.main.transform.rotation.eulerAngles.y;
             input = Quaternion.Euler(0f, y, 0f) * input;
             return input.normalized;
 
-<<<<<<< HEAD
-            //Kan (borde?) normaliseras
-=======
-
->>>>>>> c835be20f82925469b064d773a17fb084cae4dc3
         }
     }
-
-
-
-<<<<<<< HEAD
-   
-=======
-    private void Update()
-    {
-        //rb.AddForce(Input * 20f);
-
-
-    }
->>>>>>> c835be20f82925469b064d773a17fb084cae4dc3
-
 
 }
