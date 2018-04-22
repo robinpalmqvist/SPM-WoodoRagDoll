d7384f88177e844e6bc79dab449de83525be9f2a
robinpalmqvist <robin.palmqvist@gmail.com>
2018-04-22 20:18:20 +0200

    CameraLockingPlayersInView

--- WoodoRagDolls/Assets/Scripts/Testing/TestRotation.cs
+++ WoodoRagDolls/Assets/Scripts/Testing/TestRotation.cs
@@ -7,14 +7,17 @@ public class TestRotation : MonoBehaviour {
     public Transform Cube;
     private Quaternion _rotation;
     public Transform RotationalEffector;
+    public Transform PositionalVisualizer;
 	// Use this for initialization
 	void Start () {
 
         Cube.rotation = Quaternion.Euler(0, RotationalEffector.eulerAngles.y, 0);
+        PositionalVisualizer.position = Quaternion.Euler(0, RotationalEffector.eulerAngles.y, 0) * RotationalEffector.position;
 
 
-		
-	}
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
