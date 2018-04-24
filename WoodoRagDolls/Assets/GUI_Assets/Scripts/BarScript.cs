016f93c1194bd9c04e9bb593efb69c0d5b85ca46
Philip Erlandsson <Erland@eduroam-10-200-43-172.local.su.se>
2018-04-24 14:59:23 +0200

    Gui scene

---
+++ WoodoRagDolls/Assets/GUI_Assets/Scripts/BarScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour {
+
+
+	private float fillAmount;
+
+	[SerializeField]
+	private float lerpSpeed;
+
+	[SerializeField]
+	private Image content;
+
+	[SerializeField]
+	private Text valueText;
+
+	public float MaxValue { get; set;}
+
+
+	void Start () {
+
+
+	}
+	
+
+	void Update () {
+	
+		HandleBar ();
+
+	}
+
+	public float Value{
+
+		set{ 
+
+			string [] tmp = valueText.text.Split(':');
+			valueText.text = tmp [0] + ": " + value;
+			fillAmount = Map (value, 0, MaxValue, 0, 1);
+		
+		}
+	}
+
+
+	private void HandleBar(){
+	
+
+		if (fillAmount != content.fillAmount) {
+			content.fillAmount = Mathf.Lerp(content.fillAmount, fillAmount, Time.deltaTime * lerpSpeed);
+		}
+	
+	}
+
+	private float Map(float value, float inMin, float inMax, float outMin, float outMax){
+	
+		return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+
+	
+	}
+}
---
+++ WoodoRagDolls/Assets/GUI_Assets/Scripts/Stat.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Stat  {
+
+	[SerializeField]
+	private BarScript bar;
+
+	[SerializeField]
+	private float maxVal;
+
+	[SerializeField]
+	private float currentVal;
+
+
+
+	public float MaxVal{
+
+		get { 
+
+			return maxVal;
+		}
+
+		set { 			
+			this.maxVal = value;
+			bar.MaxValue = maxVal;
+		}
+	}
+
+
+
+	public float CurrentVal{
+
+		get { 
+		
+			return currentVal;
+		}
+
+		set { 
+			
+			this.currentVal = Mathf.Clamp(value, 0, MaxVal);  // gör så den inte går under 0 eller över max värdet
+			bar.Value = currentVal;
+		
+		}
+	}
+
+	public void Initialize(){
+	
+		this.MaxVal = maxVal;
+		this.CurrentVal = currentVal;
+	
+	}
+
+}
